720d8f40c4ce46eb484eadcfb27e3c27090877aa
telecasteren <127958662+telecasteren@users.noreply.github.com>
2024-08-17 20:19:45 +0200

    added forms component and split code in bitesize pieces

--- js/homepage/renderHomepage.js
+++ js/homepage/renderHomepage.js
@@ -2,13 +2,15 @@ import { fetchGames } from "/js/api/productsApi.js";
 import { UNKNOWN_KEY } from "/js/utils/general/constants.js";
 import { goToProduct } from "/js/script.js";
 import { displayContent } from "/js/homepage/indexHtml.js";
+import { alertMessage } from "/js/utils/auth/messages.js";
 
 // Adding event listener to the carousel product button:
 document.addEventListener("DOMContentLoaded", async () => {
   const getBtn = document.querySelector(".getBTN");
 
   if (!getBtn) {
-    console.log("Button not found!");
+    console.log("Button not found: ", error);
+    alertMessage("Button not found!", "warning");
     return;
   }
 
@@ -24,14 +26,18 @@ document.addEventListener("DOMContentLoaded", async () => {
         goToProduct(specificProductId);
       });
     } else {
-      getBtn.textContent = `Error upon loading button`;
       specificProductId = UNKNOWN_KEY;
-      console.log(
-        `Error ocurred while loading product-id: ${specificProductId}`
-      );
+      getBtn.addEventListener("click", () => {
+        alertMessage(
+          `Error ocurred while loading product-id: ${specificProductId}`,
+          "error"
+        );
+        getBtn.textContent = `Button: bad request`;
+      });
     }
   } catch (error) {
-    console.log("Error ocurred:", error);
+    console.log("Error ocurred: ", error);
+    alertMessage("Error ocurred: ", error, "error");
   }
 
   displayContent();
