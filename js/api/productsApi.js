b5b25043f810520ef65b60d604af39ea06d24309
telecasteren <127958662+telecasteren@users.noreply.github.com>
2024-08-19 17:33:02 +0200

    added clear search logic

--- js/api/productsApi.js
+++ js/api/productsApi.js
@@ -3,13 +3,20 @@ import { wrapper, gameUrl } from "/js/utils/general/constants.js";
 export async function fetchGames(id) {
   try {
     const url = id ? `${gameUrl}/${id}` : `${gameUrl}`;
-    const response = await fetch(url);
-    const info = await response.json();
 
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const info = await response.json();
     return info;
   } catch (error) {
     console.log("Error occurred: ", error);
-    wrapper.innerHTML = `<div class="error">An error occurred when loading the products..</div>`;
+    if (wrapper) {
+      wrapper.innerHTML = `<div class="error">An error occurred when loading the products..</div>`;
+    }
+
     throw error;
   }
 }
