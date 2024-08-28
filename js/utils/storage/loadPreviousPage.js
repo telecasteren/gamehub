c2ca10425f1be2f08939ab85fd2ce18694f654de
telecasteren <127958662+telecasteren@users.noreply.github.com>
2024-08-28 21:08:17 +0200

    loadPrevious bug fix

--- js/utils/storage/loadPreviousPage.js
+++ js/utils/storage/loadPreviousPage.js
@@ -2,19 +2,17 @@ import { PREVIOUS_PAGE_KEY } from "/js/utils/general/constants.js";
 
 // Loading previous page from localStorage:
 export function loadPreviousPage() {
-  document.addEventListener("DOMContentLoaded", () => {
-    let previousPage = localStorage.getItem(PREVIOUS_PAGE_KEY);
-    const arrowCursor = document.querySelector("#backArrow img");
+  let previousPage = localStorage.getItem(PREVIOUS_PAGE_KEY);
+  const arrowCursor = document.querySelector("#backArrow img");
 
-    if (arrowCursor) {
-      document.querySelector("#backArrow").addEventListener("click", () => {
-        if (previousPage) {
-          window.location.href = previousPage;
-        } else {
-          console.log("Error occurred upon loading previous page");
-          alert("Last browser history not found!");
-        }
-      });
-    }
-  });
+  if (arrowCursor) {
+    document.querySelector("#backArrow").addEventListener("click", () => {
+      if (previousPage) {
+        window.location.href = previousPage;
+      } else {
+        console.log("Error occurred upon loading previous page");
+        alert("Last browser history not found!");
+      }
+    });
+  }
 }
