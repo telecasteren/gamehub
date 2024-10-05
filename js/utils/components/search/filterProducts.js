43766299830455a40f78deb8d38826cea03fe5f2
telecasteren <127958662+telecasteren@users.noreply.github.com>
2024-10-05 09:01:02 +0200

    minor search field adjustments

--- js/utils/components/search/filterProducts.js
+++ js/utils/components/search/filterProducts.js
@@ -43,23 +43,3 @@ export async function gamesHTML(games) {
     throw error;
   }
 }
-
-// export async function gamesHTML(games) {
-//   wrapper.innerHTML = "";
-
-//   try {
-//     const searchTerm = localStorage.getItem(SEARCH_KEY) || "";
-//     const filteredProducts = games.filter((game) =>
-//       game.title.toLowerCase().includes(searchTerm.toLowerCase())
-//     );
-
-//     if (filteredProducts.length) {
-//       filteredProducts.forEach((game) => renderProduct(game));
-//     } else {
-//       noResultMessage();
-//     }
-//   } catch (error) {
-//     console.error("Error occurred: ", error);
-//     wrapper.innerHTML = `<div class="error">An error occurred when loading products..</div>`;
-//   }
-// }
